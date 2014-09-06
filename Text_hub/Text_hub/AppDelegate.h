7d1efc3d5640a775325ca6de51e293c6f11cc134
cdx0062009 <text>
2014-09-06 15:06:38 +0800

    Initial Commit

---
+++ Text_hub/Text_hub/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Text_hub
+//
+//  Created by Mr.chen on 14-9-6.
+//  Copyright (c) 2014年 Bitom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Text_hub/Text_hub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Text_hub
+//
+//  Created by Mr.chen on 14-9-6.
+//  Copyright (c) 2014年 Bitom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
